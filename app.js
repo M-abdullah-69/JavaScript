29420db7e1696fa8e1d3944bc56ce404d2fe666c
M abdullah <abdullah.arif.9638@gmail.com>
2020-06-13 15:40:34 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// -------------------------------------------------------------------------------------
+// Chapter 01
+
+//Task01:
+// alert("Assalam O Alaikum\nWelcome to the Website")
+
+// Task02:
+// alert("Error! Please Enter A Correct Password")
+
+//Task03:
+// alert("Welcome to JS Land\nHappy Coding! ")
+
+//Task04:
+// alert("Welcome to JS Land")
+// alert("Happy Coding!")
+
+//Task05:
+// alert("Hello.. I can run JS through my web browser console")
+// console.log("Hello.. I can run JS through my web browser console")
+
+//Task06:
+// Done you can see it
+
+// Task07:
+// All done in my Website
+
+// -------------------------------------------------------------------------------------
+// Chapter 02
+
+// Task01:
+
+
+// Task02:
+
+
+// Task03:
+
+
+// Task04:
+
+
+// -------------------------------------------------------------------------------------
