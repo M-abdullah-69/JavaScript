96f27ffecd37ce1917b78a07a880bf77bd083c09
Abdullah <58034394+M-abdullah-69@users.noreply.github.com>
2020-06-15 11:29:03 -0700

    Update app.js

--- app.js
+++ app.js
@@ -28,15 +28,38 @@
 // Chapter 02
 
 // Task01:
-
-
+    // var username;
 // Task02:
-
-
+    // var myname = 'Muhammad Abdullah';
 // Task03:
-
+    // var JS = "Hello World!";
+    // alert(JS);
 
 // Task04:
+    // var bio = ['My name is Muhammad Abdullah',"I'm 20 years old","Im a web developer"];
+    // for (let i = 0; i < bio.length; i++) {
+    //     alert(bio[i]);
+    // }
 
+// Task05:
+    // var p = "pizza";
+    //   for (let i = 0; i < p.length; i++) {
+    //           document.write(p.slice(i),"<br>");
+    // }  
+
+// Task06:
+    // var email = "abdullah.arif.9638@gmail.com"
+    // alert("My email address is "+email)
+
+// Task07:
+    // var book = "A smarter way to learn JavaScript"
+    // alert("I'm trying to lear from a book "+ book)
+
+// Task08:
+    // document.write("Yah! I can write HTML contect of JavaScript...")
+
+// Task09:
+        // var a = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+        // alert(a)
 
 // -------------------------------------------------------------------------------------
